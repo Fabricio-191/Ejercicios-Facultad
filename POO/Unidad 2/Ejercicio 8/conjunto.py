d15e79631b7f5b2dfd6c83e45d0fa0c5912ec10d
Fabricio-191 <fabricio19110@gmail.com>
2022-04-25 17:46:07 -0300

    -

---
+++ POO/Unidad 2/Ejercicio 8/conjunto.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+"""
+EJERCICIO 8
+Sobrecarga de operadores
+Defina una clase “Conjunto” que represente un conjunto matemático de números enteros.
+Implemente un programa que presente un menú de opciones que permita lo siguiente:
+1- La unión de dos conjuntos, para ello sobrecargue el operador binario suma (+). Teniendo en cuenta que la unión es un nuevo conjunto que posee los elementos de ambos conjuntos, en caso de haber elementos repetidos solo aparecen una vez en el resultado. Ejemplo: Sea A={1,2,3,4} y B= {3,6,9}, A+B = {1,2,3,4,6,9}
+2- La diferencia de dos conjuntos, para ello sobrecargue el operador binario resta (-). Teniendo en cuenta que el resultado de la diferencia de conjuntos es un nuevo conjunto que posee los elementos del primer operando que no se encuentren en el segundo operando. Ejemplo: Sea A={1,2,3,4} y B= {3,6,9}, A-B = {1,2}
+3- Verificar si dos conjuntos son iguales, para ello sobrecargue el operador “==” teniendo en cuenta que dos conjuntos se consideran iguales si tienen la misma cantidad de elementos y sus valores son iguales (sin importar el orden de los elementos).
+"""
+
+class Conjunto:
+	__conjunto: list[int]
+	def __init__(self, conjunto: list[int] = []):
+		self.__conjunto = conjunto
+
+	def __str__(self):
+		return str(self.__conjunto)
+
+	def __add__(self, other: Conjunto) -> Conjunto:
+		resultado = []
+
+		for elemento in self.__conjunto:
+			resultado.append(elemento)
+	
+		for elemento in other.__conjunto:
+			if elemento not in resultado:
+				resultado.append(elemento)
+		
+		return Conjunto(resultado)
+
+	def __sub__(self, other):
+		resultado = []
+
+		for elemento in self.__conjunto:
+			if elemento not in other.__conjunto:
+				resultado.append(elemento)
+
+		return Conjunto(resultado)
+
+	def __eq__(self, other):
+		if len(self.__conjunto) != len(other.__conjunto):
+			return False
+
+		for elemento in self.__conjunto:
+			if elemento not in other.__conjunto:
+				return False
+
+		return True

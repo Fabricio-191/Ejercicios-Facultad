e893f65a9e60c40b9f07405d911410aa280657e9
Fabricio-191 <fabricio19110@gmail.com>
2022-08-23 16:03:46 -0300

    -

---
+++ EyFC II/Unidad 1/Ejercicio 18.asm
@@ -0,0 +1,39 @@
+/*
+Realizar en Assembler una subrutina parametrada que genere una tabla con los datos
+POSITIVOS.
+La dirección de comienzo de la subrutina estará dada por la etiqueta “POSITI” y los parámetros
+de la misma vendrán dados por los contenidos de los registros:
+IX: dirección de comienzo de la tabla origen de datos.
+HL: dirección de comienzo de la tabla de datos generada es BC34 h.
+En memoria se almacena una tabla de datos de 8 bits en forma consecutiva a partir de la dirección
+de memoria DCF1h; la tabla posee su marca de final de tabla en FFh.
+La tabla generada deberá poseer también su marca de final de tabla en FFh.
+Desarrollar un programa en Assembler que invoque a la subrutina del apartado anterior.
+La dirección en memoria para la carga del programa objeto es C801h.
+*/
+
+	ORG POSITI
+SUBRUTINA:
+	LD A, (IX)
+	LD (HL), A
+
+	CP FFh
+	
+	INC IX
+	INC HL
+
+	JP NZ, SUBRUTINA
+
+	RET
+	END SUBRUTINA
+
+
+
+	ORG C801h
+INICIO:
+	LD IX, DCF1h
+	LD HL, BC34h
+
+	CALL SUBRUTINA
+
+	END INICIO
---
+++ EyFC II/Unidad 1/Ejercicio 19.asm
@@ -0,0 +1,60 @@
+/*
+Realizar en Assembler una subrutina parametrada que realice la sumatoria de un conjunto de
+datos. Los datos se encuentran almacenados a partir de la dirección de memoria B34Ah, la tabla
+tiene 30 datos de 8 bits almacenados en forma consecutiva en memoria. La dirección de
+comienzo de la subrutina estará dada por la etiqueta “SUMAR” y los parámetros de la misma
+vendrán dados por los contenidos de los registros:
+• HL: dirección de comienzo de la tabla.
+• B: Contiene la cantidad de datos de la tabla.
+• E: Contiene la sumatoria del conjunto de datos.
+Desarrollar un programa en Assembler que invoque a la subrutina del apartado anterior y luego
+analice el resultado obtenido de la sumatoria. Si el resultado obtenido es menor a 100 cargar la
+tabla de datos con los valores 1, 2, 3,…, 30 (cargar la tabla de datos desde 1 hasta 30), caso
+contrario almacenar el resultado de la sumatoria en la dirección de memoria 4731h.
+La dirección en memoria para la carga del programa objeto es B4A0h.
+*/
+
+	ORG SUMAR
+SUBRUTINA:
+	LD E, 0d
+	LD A, (HL)
+
+	ADD A, E
+	LD E, A
+	INC HL
+
+	DJNZ SUBRUTINA
+	RET
+	END SUBRUTINA
+
+	ORG B4A0h
+INICIO: 
+	LD HL, B34Ah
+	LD B, 30d
+
+	PUSH HL
+	PUSH BC
+	CALL SUBRUTINA
+	POP BC
+	POP HL
+
+	CP 100d
+
+	JP M, ABC
+	
+	LD (4731h), E
+END:
+	END INICIO
+
+
+	ORG C412H
+ABC:
+	LD C, 1d
+LOOP:
+	LD (HL), C
+	INC HL
+	INC C
+
+	DJNZ LOOP
+
+	JP END

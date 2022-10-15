4a6ca417d9214f53a4590272e2b853987a787b55
Fabricio-191 <fabricio19110@gmail.com>
2022-10-15 11:28:49 -0300

    -

--- EDyA/Unidad 5/Ejercicio 4.py
+++ EDyA/Unidad 5/Ejercicio 4.py
@@ -33,33 +33,33 @@ def randomString(stringLength = 10):
 	return ''.join(random.choice(letters) for i in range(stringLength))
 
 class Bucket:
-	__items: NDArray[Any]
-	__written: NDArray[Any]
+	__array: NDArray[Any]
+	size = 3
 
-	def __init__(self, size: int) -> None:
-		self.__items = np.full(size, None)
-		self.__written = np.full(size, None)
+	def __init__(self) -> None:
+		self.__array = np.full(Bucket.size, None, dtype=tuple[int, Any])
 
-	def getEmptyPos(self):
-		"""Returns first empty pos. If not, returns None"""
-		return np.where(self.__items == None)[0][0]
+	def insert(self, key, value):
+		if self.__array[-1] is not None:
+			raise Exception('El bucket esta completo')
 
-	def write(self, pos: int, item: Any) -> None:
-		self.__items[pos] = item
-		self.__written[pos] = 1
+		for i in range(Bucket.size):
+			if self.__array[i] is None:
+				self.__array[i] = (key, value)
+				return True
 
-	def isWritten(self, pos: int):
-		return self.__written[pos]
+		return False
 
-	def __len__(self):
-		return len(self.__items)
-
-	def __getitem__(self, index: int):
-		return self.__items[index]
+	def search(self, key):
+		for pair in self.__array:
+			if pair is not None and pair[0] == key:
+				return pair[1]
+		
+		return None
 
 class TablaHash:
 	__size: int
-	__area_prim: NDArray[Any]
+	__table: NDArray[Any]
 
 	def __init__(self, size: int, usarPrimo = True) -> None:
 		self.__size = int(size / 0.7 + 1)
@@ -67,10 +67,19 @@ class TablaHash:
 		if usarPrimo:
 			self.__size = getPrimeNumber(self.__size)
 
-		self.__area_prim = np.array([
-			Bucket(np.random.randint(1, 5)) # Random bucket size
-			for _ in range(self.__size)
-		])
+		self.__table = np.empty(self.__size, dtype=Bucket)
+		for i in range(self.__size):
+			self.__table[i] = Bucket()
 
-		def __hash(self, key: int) -> int:
-			return key % self.__size					
+	def getSize(self) -> int:
+		return self.__size
+
+	def __hash(self, key: int) -> int:
+		return key % self.__size
+
+	def insert(self, key: Any, data: Any):
+		self.__table[ self.__hash(key) ].insertar(key, data)
+
+	def search(self, key: int) -> Any:
+		return self.__table[ self.__hash(key) ].search(key)
+

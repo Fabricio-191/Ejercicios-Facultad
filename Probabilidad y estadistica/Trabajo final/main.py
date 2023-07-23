010e4f8c67f0ecd0091e79621c17c24c7fad7104
Fabricio-191 <fabricio19110@gmail.com>
2023-07-23 19:01:02 -0300

    -

--- Probabilidad y estadistica/Trabajo final/main.py
+++ Probabilidad y estadistica/Trabajo final/main.py
@@ -14,21 +14,15 @@ __dirname = os.path.dirname(__file__) + '/'
 
 alpha = 0.05
 
-key = 'Height'
+key1 = 'Height'
+key2 = 'Weight'
 dataframe = pd.read_csv(__dirname + 'PesoAltura.csv') # .head(200) 
 
-# numero de intervalos
+# filter atypical values
+
+
 size = len(dataframe)
 k = round(3.3 * math.log10(size) + 1)
-
-print()
-print('Descripcion de los datos: ')
-print(dataframe.describe())
-print()
-print('Cantidad de intervalos: ', k)
-print()
-
-
 
 mean = dataframe.mean()
 std = dataframe.std()
@@ -37,24 +31,48 @@ max_value = dataframe.max()
 min_value = dataframe.min()
 
 
+# mostrar datos
+print()
+print('Tamaño de la muestra:       ', size                                          )
+print('Cantidad de intervalos:     ', k                                             )
+print()
+print('                            ', key1)
+print('Media:                      ', mean[key1]                                    )
+print('Mediana:                    ', dataframe.median()[key1]                      )
+print('Moda:                       ', dataframe.mode()[key1][0]                     )
+print('Desviacion estandar:        ', std[key1]                                     )
+print('Maximo:                     ', max_value[key1]                               )
+print('Minimo:                     ', min_value[key1]                               )
+print('Rango:                      ', max_value[key1] - min_value[key1]             )
+print('Coeficiente de variacion:   ', dataframe.std()[key1] / dataframe.mean()[key1])
+print('Coeficiente de asimetria:   ', dataframe.skew()[key1]                        )
+print('Coeficiente de curtosis:    ', dataframe.kurtosis()[key1]                    )
+print('P - 0.25:                   ', dataframe.quantile(0.25)[key1]                )
+print('P - 0.50:                   ', dataframe.quantile(0.5)[key1]                 )
+print('P - 0.75:                   ', dataframe.quantile(0.75)[key1]                )
+print()
+print('Coeficiente de correlacion: ', dataframe.corr()[key1][key2]                  )
+print()
+
+
 
 # crear los intervalos
-interval_size = (max_value[key] - min_value[key]) / k
+interval_size = (max_value[key1] - min_value[key1]) / k
 intervals = []
 for i in range(k + 1):
-	left = min_value[key] + i * interval_size
+	left = min_value[key1] + i * interval_size
 	right = left + interval_size
 
 	intervals.append({
 		'left': left,
 		'right': right,
-		'observed': len(dataframe[dataframe[key].between(left, right, inclusive='left')]) # left <= num < right
+		'observed': len(dataframe[dataframe[key1].between(left, right, inclusive='left')]) # left <= num < right
 	})
 
 # agregar el valor maximo al ultimo intervalo
 intervals[k - 1] = {
 	'left': intervals[k - 1]['left'],
-	'right': max_value[key],
+	'right': max_value[key1],
 	'observed': intervals[k - 1]['observed'] + 1
 }
 
@@ -66,7 +84,7 @@ print('Test de bondad de ajuste con distribucion normal:')
 print('H0: sigue una distribucion normal')
 print('H1: no sigue una distribucion normal')
 
-normal_test = st.normaltest(dataframe[key])
+normal_test = st.normaltest(dataframe[key1])
 
 if normal_test.pvalue > alpha:
 	print('No se rechaza H0')
@@ -87,16 +105,16 @@ def mean_test(dataframe, mean0, direccion = 'two-sided'):
 		return 'No se rechaza H0'
 
 print('Test de hipotesis para la media: ')
-print('H0: mean = 60, H1: mean < 60   ', mean_test(dataframe[key], 60, "less"))
-print('H0: mean = 20, H1: mean > 20   ', mean_test(dataframe[key], 20, "greater"))
-print('H0: mean = 45, H1: mean != 45  ', mean_test(dataframe[key], 45))
+print('H0: mean = 60, H1: mean < 60   ', mean_test(dataframe[key1], 60, "less"))
+print('H0: mean = 20, H1: mean > 20   ', mean_test(dataframe[key1], 20, "greater"))
+print('H0: mean = 45, H1: mean != 45  ', mean_test(dataframe[key1], 45))
 
 print()
 
 
 
 def variance_test(var0, direccion = "two-sided"):
-	vp = (size - 1) * var[key] / var0
+	vp = (size - 1) * var[key1] / var0
 
 	if direccion == "less":
 		ec = st.chi2.ppf(alpha, size - 1)
@@ -138,23 +156,23 @@ print()
 
 print('Intervalos de confianza: ')
 t = st.t.ppf(1 - alpha / 2, size - 1)
-err = t * std[key] / math.sqrt(size)
-print('Intervalo de confianza para la media (varianza desconocida): ({:.3f}, {:.3f})'.format(mean[key] - err, mean[key] + err))
+err = t * std[key1] / math.sqrt(size)
+print('Intervalo de confianza para la media (varianza desconocida): ({:.3f}, {:.3f})'.format(mean[key1] - err, mean[key1] + err))
 
 # interval = st.t.interval(1 - alpha, size - 1)
-# err_left = interval[0] * std[key] / math.sqrt(size)
-# err_right = interval[1] * std[key] / math.sqrt(size)
-# print('Confidence interval for the mean (unknown variance): ', mean[key] + err_left, mean[key] + err_right)
+# err_left = interval[0] * std[key1] / math.sqrt(size)
+# err_right = interval[1] * std[key1] / math.sqrt(size)
+# print('Confidence interval for the mean (unknown variance): ', mean[key1] + err_left, mean[key1] + err_right)
 
 interval = st.chi2.interval(1 - alpha, size - 1)
-left = (size - 1) * std[key] ** 2 / interval[1]
-right = (size - 1) * std[key] ** 2 / interval[0]
+left = (size - 1) * std[key1] ** 2 / interval[1]
+right = (size - 1) * std[key1] ** 2 / interval[0]
 print('Intervalo de confianza para la varianza: ({:.3f}, {:.3f})'.format(left, right))
 
 # chi_square_left = st.chi2.ppf(1 - alpha / 2, size - 1)
 # chi_square_right = st.chi2.ppf(alpha / 2, size - 1)
-# left = (size - 1) * std[key] ** 2 / chi_square_left
-# right = (size - 1) * std[key] ** 2 / chi_square_right
+# left = (size - 1) * std[key1] ** 2 / chi_square_left
+# right = (size - 1) * std[key1] ** 2 / chi_square_right
 # print('Confidence interval for the variance: ', left, right)
 
 print()
@@ -165,7 +183,7 @@ print('Test de independencia: ')
 print('H0: la altura y el peso son independientes')
 print('H1: la altura y el peso no son independientes')
 
-contingency_table = pd.crosstab(dataframe.Weight.astype(int), dataframe.Height.astype(int))
+contingency_table = pd.crosstab(dataframe[key2].astype(int), dataframe[key1].astype(int))
 
 # guardamos la tabla de contingencia en un excel
 # instalar openpyxl es necesario para que funcione (pip3 install openpyxl)
@@ -187,8 +205,8 @@ else:
 print()
 
 # regresion lineal
-result = st.linregress([dataframe.Height, dataframe.Weight])
-y_hat = result.slope * dataframe.Height + result.intercept # type: ignore
+result = st.linregress([dataframe[key1], dataframe[key2]])
+y_hat = result.slope * dataframe[key1] + result.intercept # type: ignore
 
 
 
@@ -197,23 +215,23 @@ fig, axs = plt.subplots(2, 2)
 
 
 axs[0, 0].set_title('Histograma')
-axs[0, 0].hist(dataframe[key], bins = k, color = 'blue', edgecolor = 'black', alpha = 0.5)
+axs[0, 0].hist(dataframe[key1], bins = k, color = 'blue', edgecolor = 'black', alpha = 0.5)
 
-x = np.linspace(min_value[key], max_value[key], 1000)
-y = st.norm.pdf(x, mean[key], std[key]) * size * interval_size
+x = np.linspace(min_value[key1], max_value[key1], 1000)
+y = st.norm.pdf(x, mean[key1], std[key1]) * size * interval_size
 axs[0, 0].plot(x, y, color = 'red', label = 'Distribucion normal')
 
 
 axs[0, 1].set_title('Regresion lineal')
-axs[0, 1].scatter(dataframe.Height, dataframe.Weight, s = 0.1)
-axs[0, 1].plot(dataframe.Height, y_hat, color = 'red')
+axs[0, 1].scatter(dataframe[key1], dataframe[key2], s = 0.1)
+axs[0, 1].plot(dataframe[key1], y_hat, color = 'red')
 
 
 axs[1, 0].set_title('Intervalos de confianza para la media (hacer zoom)')
-axs[1, 0].scatter(dataframe.index, dataframe[key], s = 0.1)
-axs[1, 0].plot(dataframe.index, [mean[key]] * size, color = 'red', label = 'Media')
-axs[1, 0].plot(dataframe.index, [mean[key] + err] * size, color = 'green', label = 'Media + error')
-axs[1, 0].plot(dataframe.index, [mean[key] - err] * size, color = 'green', label = 'Media - error')
+axs[1, 0].scatter(dataframe.index, dataframe[key1], s = 0.1)
+axs[1, 0].plot(dataframe.index, [mean[key1]] * size, color = 'red', label = 'Media')
+axs[1, 0].plot(dataframe.index, [mean[key1] + err] * size, color = 'green', label = 'Media + error')
+axs[1, 0].plot(dataframe.index, [mean[key1] - err] * size, color = 'green', label = 'Media - error')
 
 
 axs[1, 1].set_title('Test de independencia')

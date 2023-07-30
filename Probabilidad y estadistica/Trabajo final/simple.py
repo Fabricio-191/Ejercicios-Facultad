924960dca14f819c43d514c5732f4a77c842942b
Fabricio-191 <fabricio19110@gmail.com>
2023-07-30 18:49:45 -0300

    -

--- Probabilidad y estadistica/Trabajo final/simple.py
+++ Probabilidad y estadistica/Trabajo final/simple.py
@@ -9,45 +9,9 @@ import numpy
 # cdf: cumulative distribution function (area under the curve) (number -> cumulative probability)
 # ppf: percent point function (inverse of cdf — percentiles) (cumulative probability -> number)
 
-alpha = 0.05
-filterOutliers = False
-dataset = 1
-k = None
-
-if dataset == 1:
-	dataframe = pandas.read_csv(os.path.dirname(__file__) + '/PesoAltura.csv') # .head(200) 
-elif dataset == 2:
-	dataframe = pandas.DataFrame({
-		# round to two decimals
-		'altura': numpy.round_(st.norm.rvs(172.7, 4.83, 25000), 2), # type: ignore
-		'peso': numpy.round_(st.norm.rvs(57.64, 5.3, 25000), 2) # type: ignore
-	})
-elif dataset == 3:
-	# peso = (0.551 ± 0.006) * altura + (-37.454 ± 1.034)
-	dataframe = pandas.DataFrame({
-		'altura': numpy.round_(st.norm.rvs(172.7, 4.83, 25000), 2) # type: ignore
-	})
-	dataframe['peso'] = numpy.round_( # type: ignore
-		(0.551 * dataframe.altura) - 37.454 + st.norm.rvs(0, 5.3, 25000), 2
-	)
-else:
-	raise Exception('Invalid dataset')
-
-
-
-if filterOutliers:
-	IQR = dataframe.altura.quantile(0.75) - dataframe.altura.quantile(0.25)
-	dataframe = dataframe[dataframe.altura.between(
-		dataframe.altura.quantile(0.25) - 1.5 * IQR,
-		dataframe.altura.quantile(0.75) + 1.5 * IQR
-	)]
-
-	IQR = dataframe.peso.quantile(0.75) - dataframe.peso.quantile(0.25)
-	dataframe = dataframe[dataframe.peso.between(
-		dataframe.peso.quantile(0.25) - 1.5 * IQR,
-		dataframe.peso.quantile(0.75) + 1.5 * IQR
-	)]
-
+alpha = 0.00000000000001 # 99.99999999999999% de confianza
+alpha = 0.01
+dataframe = pandas.read_csv(os.path.dirname(__file__) + '/PesoAltura.csv') # .head(200) 
 
 
 size = len(dataframe)
@@ -56,46 +20,30 @@ std = dataframe.std()
 var = dataframe.var()
 max_value = dataframe.max()
 min_value = dataframe.min()
-
-if k is None:
-	k = round(3.3 * math.log10(size) + 1)
-
-
+k = round(3.3 * math.log10(size) + 1)
 
 # mostrar datos
-padding_size = 15
-significant_numbers = 5
-def a(x, padding = True):
-	if type(x) == numpy.float64:
-		x = round(x, significant_numbers)
-	elif isinstance(x, pandas.Series): # mode
-		# join all values
-		x = ', '.join([str(i) for i in x])
-		
-
-	return str(x).ljust(padding_size) if padding else str(x)
-
 print()
 print('Tamaño de la muestra:     ', size)
 print('Cantidad de intervalos:   ', k   )
 print()
-print('                      ', a('altura'                           ), a('peso'                         ))
-print('Media:                ', a(mean.altura                        ), a(mean.peso                      ))
-print('Mediana:              ', a(dataframe.altura.median()          ), a(dataframe.peso.median()        ))
-print('Moda/s:               ', a(dataframe.altura.mode()            ), a(dataframe.peso.mode()          ))
-print('Varianza:             ', a(var.altura                         ), a(var.peso                       ))
-print('Desviacion estandar:  ', a(std.altura                         ), a(std.peso                       ))
-print('Coef. de variacion:   ', a(std.altura / mean.altura           ), a(std.peso / mean.peso           ))
-print('Maximo:               ', a(max_value.altura                   ), a(max_value.peso                 ))
-print('Minimo:               ', a(min_value.altura                   ), a(min_value.peso                 ))
-print('Rango:                ', a(max_value.altura - min_value.altura), a(max_value.peso - min_value.peso))
-print('Coef. de asimetria:   ', a(dataframe.altura.skew()            ), a(dataframe.peso.skew()          ))
-print('Coef. de curtosis:    ', a(dataframe.altura.kurtosis()        ), a(dataframe.peso.kurtosis()      ))
-print('P - 0.25:             ', a(dataframe.altura.quantile(0.25)    ), a(dataframe.peso.quantile(0.25)  ))
-print('P - 0.50:             ', a(dataframe.altura.quantile(0.5)     ), a(dataframe.peso.quantile(0.5)   ))
-print('P - 0.75:             ', a(dataframe.altura.quantile(0.75)    ), a(dataframe.peso.quantile(0.75)  ))
+print('                      ', 'altura')
+print('Media:                ', mean.altura)
+print('Mediana:              ', dataframe.altura.median())
+print('Moda/s:               ', ', '.join([str(i) for i in dataframe.altura.mode()]))
+print('Varianza:             ', var.altura)
+print('Desviacion estandar:  ', std.altura)
+print('Coef. de variacion:   ', std.altura / mean.altura)
+print('Maximo:               ', max_value.altura)
+print('Minimo:               ', min_value.altura)
+print('Rango:                ', max_value.altura - min_value.altura)
+print('Coef. de asimetria:   ', dataframe.altura.skew())
+print('Coef. de curtosis:    ', dataframe.altura.kurtosis())
+print('P - 0.25:             ', dataframe.altura.quantile(0.25))
+print('P - 0.50:             ', dataframe.altura.quantile(0.5))
+print('P - 0.75:             ', dataframe.altura.quantile(0.75))
 print()
-print('Coeficiente de correlacion: ', a(dataframe.corr().altura.peso))
+print('Coeficiente de correlacion: ', dataframe.corr().altura.peso)
 print()
 
 print('Test de bondad de ajuste con distribucion normal:')
@@ -103,15 +51,7 @@ print('H0: la altura sigue una distribucion normal')
 print('H1: la altura no sigue una distribucion normal')
 normal_test = st.normaltest(dataframe.altura) # Averiguar que hace
 print('No se rechaza H0' if normal_test.pvalue > alpha else 'Se rechaza H0')
-
 print()
-print('H0: el peso sigue una distribucion normal')
-print('H1: el peso no sigue una distribucion normal')
-normal_test = st.normaltest(dataframe.peso) # Averiguar que hace
-print('No se rechaza H0' if normal_test.pvalue > alpha else 'Se rechaza H0')
-print()
-
-
 
 print('Intervalos de confianza para la altura: ')
 interval = st.t.interval(1 - alpha, size - 1, mean.altura, std.altura / math.sqrt(size))
@@ -120,18 +60,6 @@ print('Intervalo de confianza para la media:  ({:.3f}, {:.3f})'.format(interval[
 interval = st.chi2.interval(1 - alpha, size - 1)
 left = ((size - 1) * var.altura) / interval[1]
 right = ((size - 1) * var.altura) / interval[0]
-print('Intervalo de confianza para la varianza: ({:.3f}, {:.3f})'.format(left, right))
-print('Intervalo de confianza para la desviacion estandar:  ({:.3f}, {:.3f})'.format(math.sqrt(left), math.sqrt(right)))
-print()
-
-
-print('Intervalos de confianza para el peso: ')
-interval = st.t.interval(1 - alpha, size - 1, mean.peso, std.peso / math.sqrt(size))
-print('Intervalo de confianza para la media:  ({:.3f}, {:.3f})'.format(interval[0], interval[1]))
-
-interval = st.chi2.interval(1 - alpha, size - 1)
-left = ((size - 1) * var.peso) / interval[1]
-right = ((size - 1) * var.peso) / interval[0]
 print('Intervalo de confianza para la varianza: ({:.3f}, {:.3f})'.format(left, right))
 print('Intervalo de confianza para la desviacion estandar:  ({:.3f}, {:.3f})'.format(math.sqrt(left), math.sqrt(right)))
 print()
@@ -148,27 +76,20 @@ print('H0: media = 170, H1: media > 170   ', mean_test(dataframe.altura, 20, 'gr
 print('H0: media = 180, H1: media != 180  ', mean_test(dataframe.altura, 45, 'two-sided'))
 print()
 
-print('Test de hipotesis para la media del peso: ')
-print('H0: media = 70, H1: media < 70   ', mean_test(dataframe.peso, 70, 'less'))
-print('H0: media = 70, H1: media > 70   ', mean_test(dataframe.peso, 70, 'greater'))
-print('H0: media = 50, H1: media != 50  ', mean_test(dataframe.peso, 50, 'two-sided'))
-print()
-
 
 def variance_test(var0, direccion):
-	# vp = ((size - 1) * var.altura) / var0
+	vp = ((size - 1) * var.altura) / var0
 
-	# if direccion == 'greater':
-	# 	result = st.chi2.ppf(1 - alpha, size - 1)
-	# 	return 'Se rechaza H0' if vp > result else 'No se rechaza H0'
-	# elif direccion == 'less':
-	# 	result = st.chi2.ppf(alpha, size - 1)
-	# 	return 'Se rechaza H0' if vp < result else 'No se rechaza H0'
-	# elif direccion == 'two-sided':
-	# 	result_left = st.chi2.ppf(alpha / 2, size - 1)
-	# 	result_right = st.chi2.ppf(1 - alpha / 2, size - 1)
-	# 	print(result_left, result_right, vp)
-	# 	return 'Se rechaza H0' if vp < result_left or vp > result_right else 'No se rechaza H0'
+	if direccion == 'greater':
+		result = st.chi2.ppf(1 - alpha, size - 1)
+		return 'Se rechaza H0' if vp > result else 'No se rechaza H0'
+	elif direccion == 'less':
+		result = st.chi2.ppf(alpha, size - 1)
+		return 'Se rechaza H0' if vp < result else 'No se rechaza H0'
+	elif direccion == 'two-sided':
+		result_left = st.chi2.ppf(alpha / 2, size - 1)
+		result_right = st.chi2.ppf(1 - alpha / 2, size - 1)
+		return 'Se rechaza H0' if vp < result_left or vp > result_right else 'No se rechaza H0'
 
 	vp = ((size - 1) * var.altura) / var0
 	p_value = st.chi2.cdf(vp, size - 1)
@@ -184,36 +105,16 @@ print('H0: var = 25,   H1: var < 25      ', variance_test(25, 'less'))
 print('H0: var = 23.5, H1: var != 23.5   ', variance_test(23.5, 'two-sided'))
 print()
 
-print('Test de hipotesis para la varianza del peso: ')
-print('H0: var = 30,   H1: var > 30      ', variance_test(30, 'greater'))
-print('H0: var = 20,   H1: var < 20      ', variance_test(20, 'less'))
-print('H0: var = 28,   H1: var != 28     ', variance_test(27.97649, 'two-sided'))
-print()
-
-
 
 print('Test de independencia: ')
 print('H0: la altura y el peso son independientes')
 print('H1: la altura y el peso no son independientes')
-
-
-# test de independencia usando el coeficiente de correlacion de Spearman usando 5 intervalosde igual tamaño
 contingency_table = pandas.crosstab(
 	pandas.cut(dataframe.altura, bins = 5, labels = ['Muy bajo', 'Bajo', 'Normal', 'Alto', 'Muy alto']),
 	pandas.cut(dataframe.peso, bins = 5, labels = ['Muy bajo', 'Bajo', 'Normal', 'Alto', 'Muy alto'])
 )
 result = st.chi2_contingency(contingency_table)
-print( 'No se rechaza H0' if result.pvalue > alpha else 'Se rechaza H0') # type: ignore
-
-# # test de independencia usando el coeficiente de correlacion de Pearson
-# result = st.pearsonr(dataframe.altura, dataframe.peso) # Averiguar que hace
-# print("Pearson's: " + 'No se rechaza H0' if result.pvalue > alpha else 'Se rechaza H0') # type: ignore
-
-# # test de independencia usando el coeficiente de correlacion de Spearman usando intervalos de 1 en 1
-# contingency_table = pandas.crosstab(dataframe.altura.astype(int), dataframe.peso.astype(int))
-# result = st.chi2_contingency(contingency_table)
-# print('No se rechaza H0' if result.pvalue > alpha else 'Se rechaza H0') # type: ignore
-
+print('No se rechaza H0' if result.pvalue > alpha else 'Se rechaza H0') # type: ignore
 print()
 
 
@@ -231,7 +132,6 @@ y_hat_min = (result.slope - result.stderr) * dataframe.altura + result.intercept
 
 
 
-
 fig, axs = plt.subplots(2, 3)
 
 
@@ -245,6 +145,8 @@ axs[0, 0].plot(x, y, color = 'red', label = 'Distribucion normal')
 
 st.probplot(dataframe.altura, dist='norm', plot=axs[0, 1])
 axs[0, 1].set_title('QQ plot')
+axs[0, 1].set_xlabel('')
+axs[0, 1].set_ylabel('')
 
 
 axs[0, 2].set_title('Regresion lineal de la altura')
